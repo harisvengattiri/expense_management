49310fb8ba44c5095ef39d4f37cf4196a157626f
Haris <harisvengattiri@gmail.com>
2025-03-24 23:33:33 +0530

    fix: corrected getIP function

--- classes/Auth.php
+++ classes/Auth.php
@@ -46,29 +46,14 @@ class Auth extends DatabaseConnection {
         $this->getConnection()->query($sql);
     }
 
-    function getIPDetails() {
-        $ip=$_SERVER['REMOTE_ADDR'];
-        if($ip != '::1') {
-        $ip_details=json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
-        $ip_city=$ip_details->city;
-        $ip_region=$ip_details->region;
-        $ip_country=$ip_details->country;
-          $ip_location="$ip_city, $ip_region, $ip_country";
-          return $ip_location;
-        } else {
-          $ip_location = 'Accessing locally with out IP';
-          return $ip_location;
-        }
-    }
-
     function successLog($userDetails) {
-        $iplocation = $this->getIPDetails();
+        $iplocation = getIPDetails();
         $this->setUserSession($userDetails);
         $this->trackLoginAttempt($userDetails['username'], $iplocation, 'success');
     }
 
     function failedLog($user) {
-        $iplocation = $this->getIPDetails();
+        $iplocation = getIPDetails();
         $this->trackLoginAttempt($user, $iplocation, 'failed');
         session_destroy();
     }
