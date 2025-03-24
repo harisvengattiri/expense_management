eedb52315f4c66eb57fea27369c72b16abfdb3aa
Haris <harisvengattiri@gmail.com>
2025-03-24 16:45:19 +0530

    fix: getIp function moved to auth class

--- classes/Auth.php
+++ classes/Auth.php
@@ -46,14 +46,29 @@ class Auth extends DatabaseConnection {
         $this->getConnection()->query($sql);
     }
 
+    function getIPDetails() {
+        $ip=$_SERVER['REMOTE_ADDR'];
+        if($ip != '::1') {
+        $ip_details=json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
+        $ip_city=$ip_details->city;
+        $ip_region=$ip_details->region;
+        $ip_country=$ip_details->country;
+          $ip_location="$ip_city, $ip_region, $ip_country";
+          return $ip_location;
+        } else {
+          $ip_location = 'Accessing locally with out IP';
+          return $ip_location;
+        }
+      }
+
     function successLog($userDetails) {
-        $iplocation = '1.0.0.0';
+        $iplocation = $this->getIPDetails();
         $this->setUserSession($userDetails);
         $this->trackLoginAttempt($userDetails['username'], $iplocation, 'success');
     }
 
     function failedLog($user) {
-        $iplocation = '1.0.0.0';
+        $iplocation = $this->getIPDetails();
         $this->trackLoginAttempt($user, $iplocation, 'failed');
         session_destroy();
     }
